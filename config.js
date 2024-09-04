4a7fb049411a6b749e4b627c38ff3b354baff877
zeusnew <160241147+zeusnew@users.noreply.github.com>
2024-09-03 23:34:03 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "HF1UQADa#XZS9YQGF7_ZG0Qm2ltEEopjKExAN-k1SNOniYaT1-os",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ? 'true' : process.env.AUTO_READ_STATUS,    
-MONGODB: process.env.MONGODB || "mongodb://mongo:YwaWQTJTuUHeFHPSoTZvLunISgkvYvsE@autorack.proxy.rlwy.net:12762"
+SESSION_ID: process.env.SESSION_ID === undefined ? 'qBVhTJCa#tKDA8myMfKGQfFtjlTObxvuJ00DVqDe8kI2Bn2z6lUU' : process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3c64b5608dd82d33dabe8.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am King-Kevin-Md I am alive now...!",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
