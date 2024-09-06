aa0f03ce76e0dfb24a6d5b6097762303711b9df4
Cocdas <138351304+Cocdas@users.noreply.github.com>
2024-09-06 20:42:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'pvlhnTCJ#_a4gHd-B49ajhcUayev76BEE7D3l2MV6qUUB18paeGo' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'TCRBUTSA#ymUgqvBjfVmnhV5MH0O07WrajEbOTjPcdYr4e35vG78' : process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3c64b5608dd82d33dabe8.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am King-dizer-Md I am alive now...!",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
