546ac07818566c3e7265f51e7031642fa59931ca
zeusnew <160241147+zeusnew@users.noreply.github.com>
2024-09-06 18:37:44 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? 'TCRBUTSA#ymUgqvBjfVmnhV5MH0O07WrajEbOTjPcdYr4e35vG78' : process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3c64b5608dd82d33dabe8.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/user-attachments/assets/e2d0e349-34e2-4e00-ae86-545a32e26b14",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am King-dizer-Md I am alive now...!",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
