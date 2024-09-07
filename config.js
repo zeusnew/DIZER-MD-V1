75977a03ec96f10aa79e5d361f8d0fb6d0b72337
zeusnew <160241147+zeusnew@users.noreply.github.com>
2024-09-06 21:51:45 -0700

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,4 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? 'TCRBUTSA#ymUgqvBjfVmnhV5MH0O07WrajEbOTjPcdYr4e35vG78' : process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/a1519f1a766f7b0ed86e6.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am King-dizer-Md I am alive now...!",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
