ad35b10c5107e59709bc835ad0d5dba19cbfdf71
zeusnew <160241147+zeusnew@users.noreply.github.com>
2024-09-14 02:48:09 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
@@ -5,7 +6,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'TCRBUTSA#ymUgqvBjfVmnhV5MH0O07WrajEbOTjPcdYr4e35vG78' : process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/a1519f1a766f7b0ed86e6.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am King-dizer-Md I am alive now...!",
+SESSION_ID: process.env.SESSION_ID || "TCRBUTSA#ymUgqvBjfVmnhV5MH0O07WrajEbOTjPcdYr4e35vG78",
+MONGODB: process.env.MONGODB || "",
 };
