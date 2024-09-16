407ca5099b796b8ab1a0f820e92638dd30ee7099
zeusnew <160241147+zeusnew@users.noreply.github.com>
2024-09-15 22:39:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const ownerNumber = ['+94712335744']
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
 const sessdata = config.SESSION_ID
-const filer = File.fromURL(https://mega.nz/file/${sessdata})
+const filer = File.fromURL(`https://mega.nz/file/${sessdata}`);
 filer.download((err, data) => {
 if(err) throw err
 fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
